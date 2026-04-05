f6f993ec115d1f907f0affc9f99a6e71720be3de
vermf <vermf@VIJAYA999_80021>
2026-04-04 22:14:22 -0500

    Added validations on Tenant.cs file properties and fields.

--- PropertyRentalManagement.Core/Entities/Tenant.cs
+++ PropertyRentalManagement.Core/Entities/Tenant.cs
@@ -3,18 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 
-namespace PropertyRentalManagement.Core.Entities;
-
-public class Tenant
+namespace PropertyRentalManagement.Core.Entities
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string PhoneNumber { get; set; } = string.Empty;
 
-    public ICollection<Lease> Leases { get; set; } = new List<Lease>();
-    public ICollection<MaintenanceRequest> MaintenanceRequests { get; set; } = new List<MaintenanceRequest>();
+    public class Tenant
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(150)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(30)]
+        public string PhoneNumber { get; set; } = string.Empty;
+
+
+        public ICollection<Lease> Leases { get; set; } = new List<Lease>();
+        public ICollection<MaintenanceRequest> MaintenanceRequests { get; set; } = new List<MaintenanceRequest>();
+
+    }
+
 }
