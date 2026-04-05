3146e6f1d62b9709a17c2c3191cdc54cfbfb63db
vermf <vermf@VIJAYA999_80021>
2026-04-05 16:46:15 -0500

    Restored validations on landlord.cs file , added additional validations for phone number and email address formatting.

--- PropertyRentalManagement.Core/Entities/Landlord.cs
+++ PropertyRentalManagement.Core/Entities/Landlord.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 
 namespace PropertyRentalManagement.Core.Entities;
 
 public class Landlord
 {
     public int Id { get; set; }
+
+     [Required(ErrorMessage = "Landlord full name is required.")]
+     [StringLength(75)]
     public string FullName { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "Email address is required")]
+    [EmailAddress(ErrorMessage = "Invalid email address format")]
     public string Email { get; set; } = string.Empty;
+
+
+    [Required(ErrorMessage = "Phone number is required.")]
+    [Phone(ErrorMessage = "Invalid phone number format.")]
+    // Optional: Use Regex for a specific 10-digit format
+   //  [RegularExpression(@"^(\d{10})$", ErrorMessage = "Please enter a 10-digit number.")]
+   
+    [RegularExpression(@"((\(\d{3}\) ?)|(\d{3}-))?\d{3}-\d{4}", ErrorMessage = "Invalid Phone Number!, required format is 111-222-9999")]
+
     public string PhoneNumber { get; set; } = string.Empty;
 
     public ICollection<Property> Properties { get; set; } = new List<Property>();
